b9048e695af08f95e6e2e3b8415b42fc9511437e
Gabriel <gabrieleleuterio990@gmail.com>
2025-03-31 21:42:17 -0300

    Exemplo de leitura de dados com a classe Scanner

--- src/Teste/TestaOlaMundo.java
+++ src/Teste/TestaOlaMundo.java
@@ -1,10 +1,22 @@
 package Teste;
 
+import java.util.Scanner;
+
 public class TestaOlaMundo {
     public static void main(String[] args) {
         //<nome classe> <nome do objeto> = new <nome classe>
         OlaMundo xyz = new OlaMundo();
         xyz.imprimirDados();
         xyz.imprimir("Fernando",18);
+
+        // Criação de um objeto para leitura de dados
+        Scanner leDados = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = leDados.nextLine();
+
+        System.out.print("Digite seu idade: ");
+        int valor = leDados.nextInt();
+
+        xyz.imprimir(nome, valor);
     }
 }
